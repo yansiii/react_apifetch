83881c71674bcf9e293dd0516e7633a610037567
yansiii <isotiyayansi@gmail.com>
2024-08-31 13:08:15 +0530

    a

---
+++ src/components/Apps.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+
+import { useEffect, useState } from 'react'
+
+
+
+const Apps=()=> {
+  
+  const[fake, setFake]  = useState([]);
+  console.log(fake)
+
+  useEffect(()=>{
+    fakestore();
+  },[])
+
+  const fakestore=async()=>{
+    const response=await fetch('https://fakestoreapi.com/products')
+    // console.log(response)
+    const jsonData =await response.json();
+    // console.log(jsonData)
+    setFake(jsonData)
+  }
+  // fakestore()
+
+  return (
+    <>
+     <h2>Fake Api store</h2>
+      <div className='container'>
+        {fake.map((values)=>{
+         return(
+         <>
+         <div className='box'>
+          <div className='content'>
+           <h5>{values.title}</h5>
+           <h3>Price : {values.price}</h3>
+           <p>{values.description} </p>
+          </div>
+          <img src={values.image} alt="" />
+        </div>
+         </>
+         )
+        })}
+      
+    
+      </div>
+    </>
+  )
+}
+
+export default Apps
